79aacee29bf662324cb9127d1e437d1812241735
SandraIpiales <seipiales@uce.edu.ec>
2024-01-10 12:20:46 -0500

    Juego Ahorcado PASO 0

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -1,1 +1,11 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+esMayuscula = function(caracter){
+    let codAscci= caracter.charCodeAt("0");
+    let letraMays;
+    if(codAscci>=65&&codAscci<=90){
+        letraMays= true;
+    }else{
+        letraMays= false;
+    }
+    return letraMays;
+}
