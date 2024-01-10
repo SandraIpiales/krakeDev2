ec9f6a0067597f23736b8dfff507bd54cd04c616
SandraIpiales <seipiales@uce.edu.ec>
2024-01-10 09:19:31 -0500

    se agrego cadenaInvertida

---
+++ bucles/ejercicioCadena.js
@@ -0,0 +1,39 @@
+ejercutarPrueba1= function(){
+    let mensaje;
+    mensaje=recuperarTexto("txtCadena");
+    recorrerCadena(mensaje);
+}
+
+ejercutarPrueba2= function(){
+    let mensaje;
+    mensaje=recuperarTexto("txtCadena");
+    
+    mostrarTexto('lblResultado', recorrerCadenaInversa(mensaje));
+}
+
+recorrerCadena = function(cadena){
+    //0123
+    //Juan
+    let caracter;
+    for(let posicion = 0; posicion<cadena.length;posicion++){
+        caracter=cadena.charAt(posicion);
+        console.log("Caracter: "+caracter+ "posicion "+posicion);
+    }
+
+    for(let posicion = 0; posicion<=cadena.length-1;posicion++){
+        caracter=cadena.charAt(posicion);
+        console.log("CARACTER: "+caracter+ "posicion "+posicion);
+    }
+}
+
+recorrerCadenaInversa = function(cadena){
+    let caracter;
+    let ultimaPosicion=cadena.length-1;
+    let caracterInverso="";
+    for(let posicion = ultimaPosicion; posicion>=0;posicion--){
+        caracter=cadena.charAt(posicion);
+        caracterInverso= caracterInverso+ caracter ; 
+        console.log("Caracter: "+caracter+ "posicion "+posicion);
+    }
+    return caracterInverso;
+}
