725ae39a147bd3b756037f6d896f6f2f801f773d
SandraIpiales <seipiales@uce.edu.ec>
2024-01-11 12:29:16 -0500

    Reto 44 Roles Parte 2 se agrego mostrarEmpleados

--- roles/roles.js
+++ roles/roles.js
@@ -1,12 +1,14 @@
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"1001632123",nombre:"Mario",apellido:"Martinez",sueldo:400.0}
 ]
 
 mostrarOpcionEmpleado= function(){
     mostrarComponente('divEmpleado');
     ocultarComponente('divRol');
     ocultarComponente('divResumen');
+    mostrarEmpleados();
 }
 
 mostrarOpcionRol= function(){
@@ -18,4 +20,25 @@ mostrarOpcionResumen= function(){
     mostrarComponente('divResumen');
     ocultarComponente('divEmpleado');
     ocultarComponente('divRol');
+}
+mostrarEmpleados = function(){
+    let cmpTablaEmpleados= document.getElementById('tablaEmpleados');
+    let elementoEmpleado;
+    let contenidoTabla="<table><tr>"
+    +"<th>CEDULA</th>"
+    +"<th>NOMBRE</th>"
+    +"<th>APELLIDO</th>"
+    +"<th>SUELDO</th>"
+    +"</tr>"
+    for(let i=0; i<empleados.length;i++){
+        elementoEmpleado=empleados[i];
+        contenidoTabla+=
+        "<tr><td>" + elementoEmpleado.cedula+"</td>"
+        +"<td>"+elementoEmpleado.nombre+"</td>"
+        +"<td>"+elementoEmpleado.apellido+"</td>"
+        +"<td>"+elementoEmpleado.sueldo+"</td>"
+        +"</tr>"
+    }
+    contenidoTabla+="</table>";
+    cmpTablaEmpleados.innerHTML=contenidoTabla;
 }
